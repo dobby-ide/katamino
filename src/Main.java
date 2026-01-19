cd91affba7ddadaa3d2b069a70d41d0cb17c6846
Fabio Privitera <fabio_privitera@yahoo.it>
2026-01-19 11:22:50 +0200

    add a rotate90() method. Main changes while debugging

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         Board board = new Board(4,5);
+        //board.place(PentominoColor.LGREEN.getPentomino(), 0,0);
+        //board.print();
+
+        //PentominoColor.LGREEN.getPentomino().print();
+        //board.remove(PentominoColor.LGREEN.getPentomino(), 0, 0);
+        //System.out.println(board.place(PentominoColor.RED.getPentomino(), 0,0));
+        //board.print();
+        PentominoColor.LGREEN.getPentomino().rotate90();
+        PentominoColor.LGREEN.getPentomino().rotate90();
+        PentominoColor.LGREEN.getPentomino().rotate90();
+        PentominoColor.LGREEN.getPentomino().rotate90();  //4 different positions
+        //PentominoColor.NAVYBLUE.getPentomino().rotate90();
         board.place(PentominoColor.LGREEN.getPentomino(), 0,0);
-        board.print();
-        board.remove(PentominoColor.LGREEN.getPentomino(), 0, 0);
-        System.out.println(board.place(PentominoColor.RED.getPentomino(), 0,0));
         board.print();
     }
 }
--- src/Pentomino.java
+++ src/Pentomino.java
@@ -16,11 +16,61 @@ public class Pentomino {
         this.filledCells = coords.toArray(new int[0][0]);
     }
 
+
+    //rotate90() needs to "understand" the height of a piece before it is rotated.
+    // The formula for rotating of 90deg in a matrix is as follows: (r, c) --> (c, height - 1 - r)
+    // a minRow and a maxRow will keep the height value normalized as in the method
+    public void rotate90(){
+        // not an in-place routine so a new 2D array must be created to save the rotated Pentomino
+        int[][] rotated = new int[filledCells.length][2];
+
+        int minRow = Integer.MAX_VALUE;
+        int maxRow = Integer.MIN_VALUE;
+
+        for (int[] cell : filledCells){
+            minRow = Math.min(minRow, cell[0]);
+            maxRow = Math.max(maxRow, cell[0]);
+        }
+        int height = maxRow - minRow + 1;
+
+        for (int i = 0; i < filledCells.length; i++){
+            int r = filledCells[i][0];
+            int c = filledCells[i][1];
+
+            //new column and new row given by the formula
+            rotated[i][0] = c;
+            rotated[i][1] = height - 1 - r;
+        }
+
+        //normalizing so top-left is in its correct position
+//        int minRotRow = Integer.MAX_VALUE;
+//        int minRotCol = Integer.MAX_VALUE;
+//        for (int[] cell : rotated) {
+//            minRotRow = Math.min(minRotRow, cell[0]);
+//            minRotCol = Math.min(minRotCol, cell[1]);
+//        }
+//        for (int[] cell : rotated) {
+//            cell[0] -= minRotRow;
+//            cell[1] -= minRotCol;
+//        }
+
+        for (int i = 0; i < filledCells.length; i++) {
+            filledCells[i][0] = rotated[i][0];
+            filledCells[i][1] = rotated[i][1];
+        }
+    }
+
     public int[][] getFilledCells(){
         return filledCells;
     }
 
     public PentominoColor getColor(){
         return color;
+    }
+
+    public void print() {
+        for (int[] cell : filledCells) {
+            System.out.println("(" + cell[0] + ", " + cell[1] + ")");
+        }
     }
 }
