efd176670807efc1f78ea4d96c1125c58d73147e
Fabio Privitera <fabio_privitera@yahoo.it>
2026-01-19 21:39:03 +0200

    rotate90() accepts an 2D shape array to work seamlessly with the solver

--- src/Pentomino.java
+++ src/Pentomino.java
@@ -17,47 +17,29 @@ public class Pentomino {
     }
 
 
+
     //rotate90() needs to "understand" the height of a piece before it is rotated.
     // The formula for rotating of 90deg in a matrix is as follows: (r, c) --> (c, height - 1 - r)
     // a minRow and a maxRow will keep the height value normalized as in the method
-    public void rotate90(){
-        // not an in-place routine so a new 2D array must be created to save the rotated Pentomino
-        int[][] rotated = new int[filledCells.length][2];
-
+    public int[][] rotate90(int[][] cells) {
         int minRow = Integer.MAX_VALUE;
         int maxRow = Integer.MIN_VALUE;
 
-        for (int[] cell : filledCells){
-            minRow = Math.min(minRow, cell[0]);
-            maxRow = Math.max(maxRow, cell[0]);
+        for (int[] c : cells) {
+            minRow = Math.min(minRow, c[0]);
+            maxRow = Math.max(maxRow, c[0]);
         }
         int height = maxRow - minRow + 1;
 
-        for (int i = 0; i < filledCells.length; i++){
-            int r = filledCells[i][0];
-            int c = filledCells[i][1];
-
-            //new column and new row given by the formula
+        int[][] rotated = new int[cells.length][2];
+        for (int i = 0; i < cells.length; i++) {
+            int r = cells[i][0];
+            int c = cells[i][1];
             rotated[i][0] = c;
             rotated[i][1] = height - 1 - r;
         }
 
-        //normalizing so top-left is in its correct position
-//        int minRotRow = Integer.MAX_VALUE;
-//        int minRotCol = Integer.MAX_VALUE;
-//        for (int[] cell : rotated) {
-//            minRotRow = Math.min(minRotRow, cell[0]);
-//            minRotCol = Math.min(minRotCol, cell[1]);
-//        }
-//        for (int[] cell : rotated) {
-//            cell[0] -= minRotRow;
-//            cell[1] -= minRotCol;
-//        }
-
-        for (int i = 0; i < filledCells.length; i++) {
-            filledCells[i][0] = rotated[i][0];
-            filledCells[i][1] = rotated[i][1];
-        }
+        return rotated;
     }
 
     public int[][] getFilledCells(){
