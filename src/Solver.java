f249b22d9eeff8116f5ab57271dcabca46fa49ad
Fabio Privitera <fabio_privitera@yahoo.it>
2026-01-19 17:14:21 +0200

    Solver class inititalized

--- src/Solver.java
+++ src/Solver.java
@@ -1,2 +1,23 @@
+
+import java.util.List;
+
+//the List of pieces will shrink or grow depending on which depth level the traversal is.
+
 public class Solver {
+    private final Board board;
+    private final List<PentominoColor> remainingPieces;
+
+   public Solver(Board board, List<PentominoColor> remainingPieces) {
+       this.board = board;
+       this.remainingPieces = remainingPieces;
+   }
+
+   public boolean solve(){
+
+       
+
+       return true;
+   }
+
+
 }
