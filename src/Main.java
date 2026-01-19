7d123feefe3d8091fb4e4eac65d8fe08c0bf1f48
Fabio Privitera <fabio_privitera@yahoo.it>
2026-01-19 21:36:12 +0200

    Solver class in Main, testing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Board board = new Board(4,5);
+
+        List<PentominoColor> pieces = new ArrayList<>();
+        pieces.add(PentominoColor.BLUE);
+        pieces.add(PentominoColor.CYAN);
+        pieces.add(PentominoColor.ORANGE);
+        pieces.add(PentominoColor.VIOLET);
+
+        Solver solver = new Solver(board, pieces);
+
+        if (solver.solve()){
+            board.print();
+            System.out.println("solved");
+        } else {
+            System.out.println("failed");
+        }
         //board.place(PentominoColor.LGREEN.getPentomino(), 0,0);
         //board.print();
 
@@ -8,12 +26,16 @@ public class Main {
         //board.remove(PentominoColor.LGREEN.getPentomino(), 0, 0);
         //System.out.println(board.place(PentominoColor.RED.getPentomino(), 0,0));
         //board.print();
-        PentominoColor.LGREEN.getPentomino().rotate90();
-        PentominoColor.LGREEN.getPentomino().rotate90();
-        PentominoColor.LGREEN.getPentomino().rotate90();
-        PentominoColor.LGREEN.getPentomino().rotate90();  //4 different positions
-        //PentominoColor.NAVYBLUE.getPentomino().rotate90();
-        board.place(PentominoColor.LGREEN.getPentomino(), 0,0);
-        board.print();
+        //PentominoColor.LGREEN.getPentomino().rotate90();
+        //PentominoColor.LGREEN.getPentomino().rotate90();
+        //PentominoColor.LGREEN.getPentomino().rotate90();
+
+        // after changes in Pentomino rotate90() method the filledCells are not changing but a new int[][] of cells is returned
+
+        //board.place(PentominoColor.LGREEN.getPentomino(), 0,0);
+//        Pentomino piece = PentominoColor.LGREEN.getPentomino();
+//
+//        board.place(piece,0,0);
+//        board.print();
     }
 }
